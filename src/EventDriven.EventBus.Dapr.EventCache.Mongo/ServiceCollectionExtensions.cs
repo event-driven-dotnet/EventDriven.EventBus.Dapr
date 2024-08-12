289358d0a9e5d19d062302f6a29f01b1cd1f4fc3
Sneed, Anthony <tony.sneed@icloud.com>
2024-08-12 16:58:48 -0500

    Add support for different lifetimes in DI registration methods

--- src/EventDriven.EventBus.Dapr.EventCache.Mongo/ServiceCollectionExtensions.cs
+++ src/EventDriven.EventBus.Dapr.EventCache.Mongo/ServiceCollectionExtensions.cs
@@ -20,9 +20,10 @@ public static class ServiceCollectionExtensions
     /// </summary>
     /// <param name="services">The <see cref="T:IServiceCollection" /></param>
     /// <param name="configuration">The application's <see cref="IConfiguration"/>.</param>
+    /// <param name="lifetime">Service lifetime.</param>
     /// <returns>The original <see cref="T:IServiceCollection" />.</returns>
     public static IServiceCollection AddDaprMongoEventCache(this IServiceCollection services,
-        IConfiguration configuration)
+        IConfiguration configuration, ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
         var daprEventCacheOptions = new DaprEventCacheOptions
         {
@@ -43,14 +44,36 @@ public static class ServiceCollectionExtensions
             options.EnableEventCacheCleanup = daprEventCacheOptions.EnableEventCacheCleanup;
             options.EventCacheCleanupInterval = daprEventCacheOptions.EventCacheCleanupInterval;
         });
-        services.AddSingleton<EventCacheOptions>(daprEventCacheOptions);
 
-        if (!daprEventCacheOptions.EnableEventCache) return services;
-        services.AddSingleton<IEventCache, DaprEventCache>();
-        services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
-            MongoEventHandlingRepository<IntegrationEvent>>();
-        services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
-        return services.AddMongoDbSettings<DaprStoreDatabaseSettings, EventWrapperDto>(configuration);
+        switch (lifetime)
+        {
+            case ServiceLifetime.Transient:
+                services.AddTransient<EventCacheOptions>();
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddTransient<IEventCache, DaprEventCache>();
+                services.AddTransient<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddTransient<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            case ServiceLifetime.Scoped:
+                services.AddScoped<EventCacheOptions>();
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddScoped<IEventCache, DaprEventCache>();
+                services.AddScoped<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddScoped<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            default:
+                services.AddSingleton<EventCacheOptions>(daprEventCacheOptions);
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddSingleton<IEventCache, DaprEventCache>();
+                services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+        }
+        
+        return services.AddMongoDbSettings<DaprStoreDatabaseSettings, EventWrapperDto>(configuration, lifetime);
     }
 
     /// <summary>
@@ -60,10 +83,12 @@ public static class ServiceCollectionExtensions
     /// <param name="stateStoreName">The name of the state store component to use.</param>
     /// <param name="configureDaprStoreOptions">Configure Dapr store options.</param>
     /// <param name="configureEventCacheOptions">Configure event cache options.</param>
+    /// <param name="lifetime">Service lifetime.</param>
     /// <returns>The original <see cref="T:IServiceCollection" />.</returns>
     public static IServiceCollection AddDaprMongoEventCache(this IServiceCollection services,
         string stateStoreName, Action<DaprStoreDatabaseSettings>? configureDaprStoreOptions = null,
-        Action<DaprEventCacheOptions>? configureEventCacheOptions = null)
+        Action<DaprEventCacheOptions>? configureEventCacheOptions = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
         var daprEventCacheOptions = new DaprEventCacheOptions
         {
@@ -84,18 +109,41 @@ public static class ServiceCollectionExtensions
             options.EnableEventCacheCleanup = daprEventCacheOptions.EnableEventCacheCleanup;
             options.EventCacheCleanupInterval = daprEventCacheOptions.EventCacheCleanupInterval;
         });
-        services.AddSingleton<EventCacheOptions>(daprEventCacheOptions);
 
-        if (!daprEventCacheOptions.EnableEventCache) return services;
-        services.AddSingleton<IEventCache, DaprEventCache>();
-        services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
-            MongoEventHandlingRepository<IntegrationEvent>>();
-        services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
-        return services.AddDaprStoreDatabaseSettings(configureDaprStoreOptions);
+        switch (lifetime)
+        {
+            case ServiceLifetime.Transient:
+                services.AddTransient<EventCacheOptions>();
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddTransient<IEventCache, DaprEventCache>();
+                services.AddTransient<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddTransient<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            case ServiceLifetime.Scoped:
+                services.AddScoped<EventCacheOptions>();
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddScoped<IEventCache, DaprEventCache>();
+                services.AddScoped<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddScoped<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            default:
+                services.AddSingleton<EventCacheOptions>(daprEventCacheOptions);
+                if (!daprEventCacheOptions.EnableEventCache) return services;
+                services.AddSingleton<IEventCache, DaprEventCache>();
+                services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+        }
+        return services.AddDaprStoreDatabaseSettings(configureDaprStoreOptions, lifetime);
     }
 
     private static IServiceCollection AddDaprStoreDatabaseSettings(this IServiceCollection services,
-        Action<DaprStoreDatabaseSettings>? configureDaprStoreOptions = null)
+        Action<DaprStoreDatabaseSettings>? configureDaprStoreOptions = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+
     {
         var databaseSettings = new DaprStoreDatabaseSettings();
         if (configureDaprStoreOptions != null)
@@ -103,11 +151,30 @@ public static class ServiceCollectionExtensions
             configureDaprStoreOptions(databaseSettings);
             services.Configure(configureDaprStoreOptions);
         }
-        return services.AddSingleton(_ =>
+
+        switch (lifetime)
         {
-            var client = new MongoClient(databaseSettings.ConnectionString);
-            var database = client.GetDatabase(databaseSettings.DatabaseName);
-            return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
-        });
+            case ServiceLifetime.Transient:
+                return services.AddTransient(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+            case ServiceLifetime.Scoped:
+                return services.AddScoped(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+            default:
+                return services.AddSingleton(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+        }
     }
 }
--- src/EventDriven.EventBus.EventCache.Mongo/ServiceCollectionExtensions.cs
+++ src/EventDriven.EventBus.EventCache.Mongo/ServiceCollectionExtensions.cs
@@ -58,10 +58,12 @@ public static class ServiceCollectionExtensions
     /// <param name="services">The <see cref="T:IServiceCollection" /></param>
     /// <param name="configureMongoStoreOptions">Configure Mongo store options.</param>
     /// <param name="configureEventCacheOptions">Configure event cache options.</param>
+    /// <param name="lifetime">Service lifetime.</param>
     /// <returns>The original <see cref="T:IServiceCollection" />.</returns>
     public static IServiceCollection AddMongoEventCache(this IServiceCollection services,
         Action<MongoStoreDatabaseSettings>? configureMongoStoreOptions = null,
-        Action<MongoEventCacheOptions>? configureEventCacheOptions = null)
+        Action<MongoEventCacheOptions>? configureEventCacheOptions = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
         var eventCacheOptions = new MongoEventCacheOptions
         {
@@ -78,18 +80,41 @@ public static class ServiceCollectionExtensions
             options.EnableEventCacheCleanup = eventCacheOptions.EnableEventCacheCleanup;
             options.EventCacheCleanupInterval = eventCacheOptions.EventCacheCleanupInterval;
         });
-        services.AddSingleton<EventCacheOptions>(eventCacheOptions);
 
-        if (!eventCacheOptions.EnableEventCache) return services;
-        services.AddSingleton<IEventCache, MongoEventCache>();
-        services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
-            MongoEventHandlingRepository<IntegrationEvent>>();
-        services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
-        return services.AddMongoStoreDatabaseSettings(configureMongoStoreOptions);
+        switch (lifetime)
+        {
+            case ServiceLifetime.Transient:
+                services.AddTransient<EventCacheOptions>();
+                if (!eventCacheOptions.EnableEventCache) return services;
+                services.AddTransient<IEventCache, MongoEventCache>();
+                services.AddTransient<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddTransient<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            case ServiceLifetime.Scoped:
+                services.AddScoped<EventCacheOptions>();
+                if (!eventCacheOptions.EnableEventCache) return services;
+                services.AddScoped<IEventCache, MongoEventCache>();
+                services.AddScoped<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddScoped<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+            default:
+                services.AddSingleton<EventCacheOptions>(eventCacheOptions);
+                if (!eventCacheOptions.EnableEventCache) return services;
+                services.AddSingleton<IEventCache, MongoEventCache>();
+                services.AddSingleton<IEventHandlingRepository<IntegrationEvent>,
+                    MongoEventHandlingRepository<IntegrationEvent>>();
+                services.AddSingleton<IDocumentRepository<EventWrapperDto>, DocumentRepository<EventWrapperDto>>();
+                break;
+        }
+        
+        return services.AddMongoStoreDatabaseSettings(configureMongoStoreOptions, lifetime);
     }
 
     private static IServiceCollection AddMongoStoreDatabaseSettings(this IServiceCollection services,
-        Action<MongoStoreDatabaseSettings>? configureMongoStoreOptions = null)
+        Action<MongoStoreDatabaseSettings>? configureMongoStoreOptions = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
         var databaseSettings = new MongoStoreDatabaseSettings();
         if (configureMongoStoreOptions != null)
@@ -97,11 +122,30 @@ public static class ServiceCollectionExtensions
             configureMongoStoreOptions(databaseSettings);
             services.Configure(configureMongoStoreOptions);
         }
-        return services.AddSingleton(_ =>
+
+        switch (lifetime)
         {
-            var client = new MongoClient(databaseSettings.ConnectionString);
-            var database = client.GetDatabase(databaseSettings.DatabaseName);
-            return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
-        });
+            case ServiceLifetime.Transient:
+                return services.AddTransient(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+            case ServiceLifetime.Scoped:
+                return services.AddScoped(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+            default:
+                return services.AddSingleton(_ =>
+                {
+                    var client = new MongoClient(databaseSettings.ConnectionString);
+                    var database = client.GetDatabase(databaseSettings.DatabaseName);
+                    return database.GetCollection<EventWrapperDto>(databaseSettings.CollectionName);
+                });
+        }
     }
 }
